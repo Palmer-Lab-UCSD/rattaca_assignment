f2c5c5a4fe7d15493c865437b727966b75854eb2
Benjamin Johnson <bbjohnson@health.ucsd.edu>
2025-10-31 14:04:23 -0700

    conditional steps to format or update prior results

--- rattaca_assign/main.py
+++ rattaca_assign/main.py
@@ -12,39 +12,84 @@ def main(args=None):
     print('---- RATTACA Assignment ----')
     print('---------------------------- \n')
 
-    print(f'Request files:')
-    for req in args.requests:
-        print(f'\t{req}')
-    print(f'Colony dataframe: {args.colony_dataframe[0]}')
-    if args.predictions:
-        print(f'Predictions file: {args.predictions[0]}')
-    print('\n')
-        
-    # run the assignment algorithm
-    assignments = run_assignments(args)
+    do_step = args.step
 
-    # save results to file
-    if args.output_prefix:
-        
-        # assignment files: per-request and all requests
-        assignments_df = output_assignment_results(
-            args = args, 
-            assigned_requests = assignments, 
-            output_prefix = os.path.join(args.output_dir[0], args.output_prefix[0])
-        )
-        
+    # if conducting new assignments
+    if do_step == 'new_assignments':
+
+        print(f'Request files:')
+        for req in args.requests:
+            print(f'\t{req}')
+        print(f'Colony dataframe: {args.colony_dataframe[0]}')
+        if args.predictions:
+            print(f'Predictions file: {args.predictions[0]}')
+        print('\n')
+  
+        # run the assignment algorithm
+        assignments = run_assignments(args)
+
+        # save results to file
+        if args.output_prefix:
+            
+            # assignment files: per-request and all requests
+            assignments_df = output_assignment_results(
+                args = args, 
+                assigned_requests = assignments, 
+                output_prefix = os.path.join(args.output_dir[0], \
+                    args.output_prefix[0])
+            )
+            
+            # RATTACA requests results files: assignments + predictions
+            output_assignment_preds(
+                requests = args.requests,
+                assignments = assignments_df,
+                preds = args.predictions[0],
+                outdir = args.output_dir[0],
+                request_map = args.request_map[0])
+
+        else:
+            print('\nNo results saved to file. Set [-o OUTPUT_PREFIX] in the command line to save results \n')
+
+        print('\nRATTACA assignment complete! \n')
+
+
+    # if updating previously proposed assignments
+    elif do_step == 'update_assignments':
+
+        print('----------------------------')
+        print('--- Updating Assignments ---')
+        print('---------------------------- \n')
+    
+        print(f'Assignments file: {args.assignments[0]}')
+        print(f'Updates file: {args.updates[0]} \n')
+
+        output_dir = args.output_dir[0] if args.output_dir else None
+        print(f'output_dir: {output_dir}')
+
+        update_assignments(
+            assignments = args.assignments[0],
+            updates = args.updates[0],
+            all_requests = True,
+            outdir = output_dir)
+    
+
+    # if formatting final outputs for RATTACA assignments
+    elif do_step == 'rattaca_results':
+
+        print('-----------------------------')
+        print('--- Final RATTACA Results ---')
+        print('----------------------------- \n')
+
+        print(f'Assignments file: {args.assignments[0]}')
+
         # RATTACA requests results files: assignments + predictions
         output_assignment_preds(
-            requests = args.request_files,
-            assignments = assignments_df,
+            requests = args.requests,
+            assignments = args.assignments[0],
             preds = args.predictions[0],
             outdir = args.output_dir[0],
             request_map = args.request_map[0])
 
-    else:
-        print('\nNo results saved to file. Set [-o OUTPUT_PREFIX] in the command line to save results \n')
-
-    print('\nRATTACA assignment complete! \n')
 
 
 if __name__ == '__main__':
